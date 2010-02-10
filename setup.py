48cbb9f08372e970b6a135f6d6a7eea4bd579c9d
Paul J. Davis <paul.joseph.davis@gmail.com>
2010-02-10 11:42:13 -0500

    Version bump for minor bug fix.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 setup(
     name = 'Trawl',
-    version = '0.1',
+    version = '0.1.1',
 
     description = 'Python port of Ruby Rake',
     long_description = open(README).read(),
